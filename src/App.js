f2902edb8a8c28113c0c9ad8d8742e12466a47f4
Kojo Akine <weezer.and.you@gmail.com>
2023-07-15 20:03:50 +0900

    Fix routing issue in App.js and ImageList.js :bug:

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
 import ImageUpload from './ImageUpload';
-import ImageList from './ImageList'; // ImageList をインポートします。
+import ImageList from './ImageList';
+import ImageDetail from './ImageDetail';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function HomePage() {
+  return (
+    <>
+      <ImageUpload />
+      <ImageList />
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <ImageUpload />
-      <ImageList />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/images/:id" element={<ImageDetail />} /> {/* <-- ここを修正 */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
