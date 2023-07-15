6e234da8f04db236f0e95f1553087f74025a7c45
Kojo Akine <weezer.and.you@gmail.com>
2023-07-15 20:03:29 +0900

    Update ImageList component to use Firebase SDK v9 syntax :recycle:

--- src/ImageList.js
+++ src/ImageList.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { firestore } from './firebase';
 import { collection, getDocs } from 'firebase/firestore';
-import './ImageList.css'; // CSSファイルをインポート
+import { Link } from 'react-router-dom';
+import './ImageList.css';
 
 const ImageList = () => {
   const [images, setImages] = useState([]);
@@ -9,9 +10,10 @@ const ImageList = () => {
   useEffect(() => {
     const fetchImages = async () => {
       const imagesCollection = await getDocs(collection(firestore, 'images'));
-      setImages(imagesCollection.docs.map(doc => {
-        return {...doc.data(), id: doc.id};
-      }));
+      setImages(imagesCollection.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+      })));
     };
     fetchImages();
   }, []);
@@ -19,10 +21,12 @@ const ImageList = () => {
   return (
     <div className="image-grid">
       {images.map((image) => (
-        <div key={image.id} className="image-item">
-          <img src={image.url} alt="" className="image" />
-          <p>Timestamp: {image.timestamp?.toDate().toString()}</p>
-        </div>
+        <Link to={`/images/${image.id}`} key={image.id}>
+          <div className="image-item">
+            <img src={image.url} alt="" className="image" />
+            <p>Timestamp: {image.timestamp?.toDate().toString()}</p>
+          </div>
+        </Link>
       ))}
     </div>
   );
