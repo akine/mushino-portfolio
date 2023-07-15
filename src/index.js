44cfd39dd46470f5fe8cd93ff85fc3cb7f24b804
Kojo Akine <weezer.and.you@gmail.com>
2023-07-15 20:02:54 +0900

    Update index.js to use Firebase SDK v9 syntax :recycle:

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
+import { initializeApp } from 'firebase/app';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+// 環境変数から Firebase の設定を読み込みます
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+// Firebaseが既に初期化されていなければ初期化します
+initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
